fddd0ee73f89af7d58b1ff4cb9ef13173ef69e00
Viktor Viskov <carrergt@gmail.com>
2023-11-30 13:19:38 +0100

    Update structure for service.

--- app/services/token_service.py
+++ app/services/token_service.py
@@ -1,18 +1,25 @@
 from datetime import datetime
 
 from models import db
+from repositories import TokenRepository
 from .base_service import BaseService
 
 
-class TokenService(BaseService):        
+class TokenService(BaseService):
+    primary_repo: TokenRepository
+    
+    def __init__(self) -> None:
+        super().__init__()
+        self.primary_repo = self.token_repo
+     
     def get_by_id(self, id: int) -> db.Token | None:
-        return self.token_repo.get_by_id(id)
+        return self.primary_repo.get_by_id(id)
         
     def get_by_user_id(self, user_id: int) -> list[db.Token]:
-        return self.token_repo.get_by_user_id(user_id)
+        return self.primary_repo.get_by_user_id(user_id)
     
     def get_by_hash(self, hash: str) -> db.Token | None:
-        return self.token_repo.get_by_hash(hash)
+        return self.primary_repo.get_by_hash(hash)
         
     def create(self, user: db.User, hash: str, valid_to: datetime) -> db.Token:
         token = db.Token()
@@ -20,11 +27,11 @@ class TokenService(BaseService):
         token.hash = hash
         token.expired_at = valid_to
         
-        token_id = self.token_repo.add(token)
+        token_id = self.primary_repo.add(token)
         return self.get_by_id(token_id)
         
     def delete(self, token: db.Token) -> None:
-        self.token_repo.delete(token.id)
+        self.primary_repo.delete(token.id)
         
     def delete_expired(self, tokens: list[db.Token]) -> None:
         now = datetime.utcnow()
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,20 +1,28 @@
 from models import db
 from models import other
+from repositories import UserRepository
 from .base_service import BaseService
 
-class UserService(BaseService):       
+
+class UserService(BaseService):
+    primary_repo: UserRepository
+    
+    def __init__(self) -> None:
+        super().__init__()
+        self.primary_repo = self.user_repo
+             
     def get(self, limit: int, offset: int) -> list[db.User]:
-        return self.user_repo.get(limit=limit, offset=offset)
+        return self.primary_repo.get(limit=limit, offset=offset)
                 
     def get_by_id(self, id: int) -> db.User | None:
-        return self.user_repo.get_by_id(id)
+        return self.primary_repo.get_by_id(id)
         
     def get_by_email(self, email: str) -> db.User | None:
-        return self.user_repo.get_by_email(email)
+        return self.primary_repo.get_by_email(email)
     
     def get_by_email_and_pass(self, email: str, password: str) -> db.User | None:
         result = None
-        user = self.user_repo.get_by_email(email)
+        user = self.primary_repo.get_by_email(email)
         if user and user.password == password:
             result = user
         return result
@@ -27,14 +35,14 @@ class UserService(BaseService):
         user.email = email
         user.password = password
 
-        user_id = self.user_repo.add(user)
+        user_id = self.primary_repo.add(user)
         return self.get_by_id(user_id)
     
     def update(self, user: db.User) -> db.User | None:
-        self.user_repo.update(user)
+        self.primary_repo.update(user)
         return self.get_by_id(user.id)
     
     def delete(self, user: db.User) -> None:
-        self.user_repo.delete(user.id)
+        self.primary_repo.delete(user.id)
         
     
